92d4b095e50c904657123e829168f1417d677516
LanFP888 <41882036+LanFP888@users.noreply.github.com>
2018-10-23 00:16:27 -0400

    main logic finished

--- asset/js/func.js
+++ asset/js/func.js
@@ -46,6 +46,18 @@ function constructAPI_URL(
   return newURL;
 }
 
+//suffles the answer array
+function shuffle(arr) {
+  var j, x, i;
+  for (i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = arr[i];
+    arr[i] = arr[j];
+    arr[j] = x;
+  }
+  return arr;
+}
+
 //parsing the responseData.results
 //format in an array of objects:[{category: "", type: "", difficulty: "", question: "", correct_answer: "", type: "", incorrect_answers: []}]
 function triviaQuestionParser(arr) {
--- asset/js/game.js
+++ asset/js/game.js
@@ -11,8 +11,6 @@
 //end of game, show correct answer count, incorrect count, and option to restart the game without page reload
 
 $(document).ready(function() {
-  var gameData;
-
   $(".getData").on("click", function() {
     var gameType;
     var questionCategoryID;
@@ -28,20 +26,131 @@ $(document).ready(function() {
     $.getJSON(URL, function(responseData) {
       //the json data retrived will be converted to an object, which contains (response_code and results)
       //store the responseData object in a new gameData object for the rest of the game
-      gameData = responseData;
-      console.log(gameData);
+      console.log(responseData);
+      triviaGame.startGame(responseData.results);
       //triviaQuestionParser(gameData.results);
     });
   });
 
   $(".checkAnswer").on("click", function() {
-    var option = $("input:radio:checked", ".triviaOpt").val();
+    var option = $("input:radio:checked", ".triviaOption").val();
     //if non of the option buttons are checked, option will be undefined
     if (option !== undefined) {
-      //need to use .html so the special characters encode correctly
-      $(".triviaQuestions").html(option);
+      if (triviaGame.checkAnswer(option)) {
+        // change this to circule correct answer and get rid of other bad answers
+        // change triviaAnswer div to display timer, correct number and incorrect number
+        $(".triviaAnswer").html("You are correct!");
+        setTimeout(function() {
+          triviaGame.nextQuestion();
+        }, 1000 * 3);
+      } else {
+        $(".triviaAnswer").html(
+          "Sorry, correct answer is: " + triviaGame.correctAnswer
+        );
+        setTimeout(function() {
+          triviaGame.nextQuestion();
+        }, 1000 * 3);
+      }
     } else {
-      $(".triviaQuestions").html("Please make a selection");
+      $(".triviaQuestion").html("Please make a selection");
     }
+
+    //wait a few seconds and display the next question
   });
 });
+
+var triviaGame = {
+  gameData: [],
+  questionCount: 0,
+  questionMax: 0,
+  correctAnswer: "",
+  incorrectAnswer: [],
+  correctCount: 0,
+  incorrectCount: 0,
+
+  startGame: function(arr) {
+    //assign the variables
+    this.gameData = arr;
+    this.questionCount = 0;
+    this.correctCount = 0;
+    this.incorrectCount = 0;
+    this.questionMax = this.gameData.length;
+    this.nextQuestion();
+  },
+
+  nextQuestion: function() {
+    //if there are available questions left, continue display
+    if (this.questionCount < this.questionMax) {
+      $(".triviaAnswer").html("");
+      this.displayQuestion();
+      this.questionCount++;
+    } else {
+      $(".triviaAnswer").html("");
+      $(".triviaQuestion").html("End of Questions");
+      $(".triviaOption").empty();
+    }
+  },
+
+  displayQuestion: function() {
+    $(".triviaQuestion").html(this.gameData[this.questionCount].question);
+    //a list of options
+    this.correctAnswer = this.gameData[this.questionCount].correct_answer;
+    this.incorrectAnswer = this.gameData[this.questionCount].incorrect_answers;
+    console.log(this.correctAnswer);
+    console.log(this.incorrectAnswer);
+    var questionOption = this.incorrectAnswer.slice();
+    questionOption.push(this.correctAnswer);
+    questionOption = shuffle(questionOption);
+    //remove the previous options for new options to be added
+    $(".triviaOption").empty();
+    //list out the options
+    for (i = 0; i < questionOption.length; i++) {
+      var q1 = $("<div>");
+      q1.addClass("form-check col-12");
+      var q2 = $("<input>");
+      q2.addClass("form-check-input");
+      //radio buttons require the same name
+      q2.attr("name", "triviaOption");
+      q2.attr("type", "radio");
+      q2.attr("value", questionOption[i]);
+      q2.attr("id", questionOption[i]);
+      var q3 = $("<label>");
+      q3.attr("for", questionOption[i]);
+      q3.html("<h3>" + questionOption[i] + "</h3>");
+      $(".triviaOption").append(q1.append(q2, q3));
+    }
+  },
+
+  checkAnswer: function(answerInput) {
+    if (this.correctAnswer === answerInput) {
+      this.correctCount++;
+      return true;
+    } else {
+      this.incorrectCount++;
+      return false;
+    }
+  }
+};
+
+{
+  /* <input type="radio"
+
+  name="fontSizeControl"
+
+  id="sizeSmall"
+
+  value="small"
+
+  checked="checked" />
+
+  <label for="sizeSmall">small</label> */
+}
+
+{
+  /* <div class="form-check col-12">
+  <label class="form-check-label">
+    <input class="form-check-input" type="radio" name="exampleRadios" value="">
+      Option one is this and that&mdash;be sure to include why it's great
+                </label>
+            </div> */
+}
